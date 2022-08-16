89f3bb116f85c4af6a802dcb232aff723616b179
sirrobindoger <robindoger@gmail.com>
2022-08-15 23:57:36 -0400

    MNistdata new

--- src/loader.rs
+++ src/loader.rs
@@ -1,39 +1,41 @@
-use std::{fs::File, io::{BufReader, Read, Cursor, BufRead}};
+use std::{fs::File, io::{BufReader, Read, Cursor}};
 
 use flate2::bufread::GzDecoder;
 use byteorder::{BigEndian, ReadBytesExt};
 
 
 pub struct MNistData {
-
+    sizes: Vec<i32>,
+    data: Vec<u8>,
 }
 
 impl MNistData {
+    /// # Panics
+    /// 
+    /// Will panic if there's no idx file
+    /// # Errors
+    /// 
+    /// Will error if no idx file
     pub fn new(file: String) -> Result<MNistData, std::io::Error> {
         // loading & decompressing
-        let mut f = File::open(file)?;
-        let buffer = BufReader::new(f);
-        let mut gz = GzDecoder::new(buffer);
-        // putting decompressed buffer into vec
+        let buffer = BufReader::new( File::open(file)?);
         let mut contents: Vec<u8> = Vec::new();
-        gz.read_to_end(&mut contents)?;
-        // seeking magic number
-        let mut cursor = Cursor::new(&contents);
-        let magic_number = cursor.read_i32::<BigEndian>()?;
-
-        let mut dims: Vec<i32> = Vec::new();
-        let mut data: Vec<u8> = Vec::new();
-        match magic_number {
-            2051 => {
-                dims.push(cursor.read_i32::<BigEndian>()?);
-                dims.push(cursor.read_i32::<BigEndian>()?);
-                dims.push(cursor.read_i32::<BigEndian>()?);
-            }
-            _ => panic!(),
-        }
         
-        cursor.read_to_end(&mut data)?;
+        GzDecoder::new(buffer).read_to_end(&mut contents)?;
+        // seeking magic number
+        let mut reader = Cursor::new(&contents);
 
-        Ok(MNistData{})
+        let mut sizes: Vec<i32> = Vec::new();
+        let mut data: Vec<u8> = Vec::new();
+
+        assert_eq!(reader.read_i32::<BigEndian>()? , 2051); // check if we're using correct IDX
+
+        for _ in 0..2 {
+            sizes.push(reader.read_i32::<BigEndian>()?);
+        };
+        
+        reader.read_to_end(&mut data)?;
+
+        Ok(MNistData{sizes, data})
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,5 @@ pub mod loader;
 use loader::MNistData;
 
 fn main() {
-    MNistData::new(String::from("mnist\\train-images-idx3-ubyte.gz"))
+    MNistData::new(String::from("mnist\\train-images-idx3-ubyte.gz"));
 }
